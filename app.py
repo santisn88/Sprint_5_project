95924a48019658e569ff0434ac7cf22b391c213e
santisn88 <67713666+santisn88@users.noreply.github.com>
2024-05-29 22:15:59 -0500

    Carga en streamlit de la aplicacion

--- app.py
+++ app.py
@@ -2,15 +2,44 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-car_data = pd.read_csv("vehicles_us.csv") # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+# Leer los datos del documento csv con la ruta especificada
+car_data_df = pd.read_csv("vehicles_us.csv")
 
-if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+# Imprimir una muestra de la lectura de los datos
+st.write('Esta es mi primera aplicacion en Streamlit')
+st.write('Esta contiene informacion de vehiculos de venta en Estados Unidos')
+st.write('Muestra de los datos cargados')
 
-    # mostrar un gráfico Plotly interactivo
+st.write(car_data_df.head()) 
+
+def histogram_graph():
+    fig = px.histogram(car_data_df, x = 'odometer',
+                       labels = {
+                           'odometer' : 'Odometro',
+                       },
+                       title = 'Odometro de los vehiculos'
+    )                                                      
     st.plotly_chart(fig, use_container_width=True)
+
+def scatter_graph():
+    fig = px.scatter(car_data_df, x = 'model_year', y = 'price',
+                       labels = {
+                           'model_year' : 'Ano de los modelos',
+                           'price' : 'Precios de los modelos'
+                       },
+                       title = 'Precio de los vehiculos en relacion a los anos de modelo'
+    )                                                      
+    st.plotly_chart(fig, use_container_width=True)
+
+# Crear un botón para el histograma y scatter
+st.write("Este histograma contiene la infomracion del odometro de todos los carros de venta")
+histogram_button = st.button('Construir histograma')
+st.write("Este grafico de dispersion contiene la infomracion del precio y del ano de modelo")
+scatter_button = st.button('Construir grafico dispersion')
+
+# Al hacer clic en el botón los graficos se activan
+if histogram_button:
+    histogram_graph()
+
+if scatter_button:
+    scatter_graph()
